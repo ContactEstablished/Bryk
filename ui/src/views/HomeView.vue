cdbc3c46135de308bf38ebd922c31b4fb65a9a52
Matthew Wilson <8195809+ContactEstablished@users.noreply.github.com>
2026-04-30 09:22:36 -0400

    feat: UI starting

---
+++ ui/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Activity } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center gap-8">
+    <h1 class="text-6xl font-bold text-white">Bryk</h1>
+    <Button>
+      <Activity />
+      Get started
+    </Button>
+  </div>
+</template>
